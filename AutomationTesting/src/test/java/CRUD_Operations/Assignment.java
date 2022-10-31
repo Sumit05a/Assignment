b86f69505b0d6b207cfb75acbb236c603c0bcf29
dell <dell@DESKTOP-IUIN2QD>
2022-10-31 09:21:43 +0530

    Assign

---
+++ AutomationTesting/src/test/java/CRUD_Operations/Assignment.java
@@ -0,0 +1,41 @@
+package CRUD_Operations;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment {
+	public static void main(String[] args) throws InterruptedException {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	Actions act = new Actions(driver);
+	driver.manage().window().maximize();
+	
+//	Step 1: Go to URL
+	driver.get("http://www.htmlcanvasstudio.com/");
+	
+//	Step 2: Click on Draw a Line
+	driver.findElement(By.xpath("//input[@title='Draw a line']")).click();
+	
+//	Step 3: Draw one Horizontal Line
+	act.moveByOffset(275, 350).click().moveByOffset(400, 0).click().perform();
+	
+//	Step 4: Draw one Vertical Line(Intercepting Each Other)
+	act.moveByOffset(-200, -100).click().moveByOffset(0, 200).click().perform();
+	
+//	Step 5: Click on Draw a Rectangle
+	driver.findElement(By.xpath("//input[@title='Draw a rectangle']")).click();
+	
+//	Step 6: Draw one Rectangle
+	act.moveByOffset(-200, 50).click().moveByOffset(400, 100).click().perform();
+	
+//	Step 7: Click Eraser and Erase Horizontal Line
+	driver.findElement(By.xpath("//input[@title='Use eraser']")).click();
+	act.moveByOffset(-400, -250).clickAndHold().moveByOffset(400, 0).release().build().perform();
+	
+	}
+}
+
